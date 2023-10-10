5da459b486ea80f04c7822c7d522431f54dc0a2e
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-10-10 17:05:02 +0000

    Added tax.py and constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: October 10, 2023
+# This file contains the constants for tax.py
+
+# declaring the HST constant for the tax program
+HST = 0.15
---
+++ tax.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: October 10, 2023
+# This program calculates the tax and total using the subtotal
+import constants
+
+
+def main():
+    # get the diameter
+    print("Calculating the Tax and Total using the Subtotal")
+    subtotal = float(input("What is the subtotal of your item in dollars?"))
+
+    # calculate the subtotal, tax, and total cost
+    tax = constants.HST * subtotal
+    total = subtotal + tax
+
+    # display the tax and the total cost
+    print("Your tax is ${:.2f}.".format(tax))
+    print("Your total cost is ${:.2f}.".format(total))
+
+
+if __name__ == "__main__":
+    main()
